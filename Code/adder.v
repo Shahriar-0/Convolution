007d4d41fe398f817a15e6760a43eecbbaa96f0c
Shahriar-0 <attarshahriar@gmail.com>
2023-12-04 07:18:44 +0330

    fix adder

--- Code/adder.v
+++ Code/adder.v
@@ -1,6 +1,9 @@
 module Adder(a, b, out);
     parameter LEN = 9;
-    input[LEN - 1:0] a, b,
-    output[LEN - 1:0] out
-    assign out = a+ b;
+
+    input[LEN - 1:0] a, b;
+    output[LEN - 1:0] out;
+    
+    assign out = a + b;
+    
 endmodule
